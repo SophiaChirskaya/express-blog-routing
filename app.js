3608e9626eb0ac6cfc4237fe7f43e82d99cd72e1
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-05 18:07:07 +0100

    [feat]router/posts.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const postsRouter = require('./routers/posts');
 
 // Definizione d'uso di una cartella per i file statici
 app.use(express.static('public'));
@@ -8,11 +9,13 @@ app.use(express.static('public'));
 // Definizone di rotta home
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
-})
+});
+
+app.use("/posts", postsRouter)
 
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
     
-})
+});
---
+++ routers/posts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// Index
+router.get('/', function(req, res) {
+    res.send('Lista dei blog');
+});
+
+// Show
+router.get('/:id', function(req, res) {
+    res.send('Dettagli dei blog' + req.params.id);
+});
+
+// Create/Store
+router.post('/', function(req, res) {
+    res.send('Creazione nuovo blog');
+});
+
+// Update
+router.put('/:id', function(req, res) {
+    res.send('Modifica integrale del blog' + req.params.id);
+});
+
+// Modify
+router.patch('/:id', function(req, res) {
+    res.send('Modifica parziale del blog' + req.params.id);
+});
+
+// Destroy
+router.delete('/:id', function(req, res) {
+    res.send('Eliminazione del blog' + req.params.id);
+});
+
+module.exports = router;
