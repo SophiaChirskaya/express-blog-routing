ea453b99aebbfb4eb29005c749604b7872489153
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-06 19:50:09 +0100

    [feat]index with posts list

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,20 +1,15 @@
 const posts = require('../data/posts');
 
 function index(req, res) {
-    res.send('Lista dei posts');
-    
+    // res.send('Lista dei posts');
+    res.json(posts);
 
-    // let filteredPosts = posts;
-    // if(req.query.tag) {
-    //     filteredPosts = posts.filter(
-    //         post => post.tags.includes(req.query.tag)
-    //     );
-    // }
-    // res.json(filteredPosts);
+
     }
 
 function show(req, res) {
-    res.send('Dettagli dei blog' + req.params.id);
+    // res.send('Dettagli dei blog' + req.params.id);
+    
 
 }
 
